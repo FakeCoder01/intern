303a74b5002edba6e8b846ecb316a6bdb8de9d00
Ayon Samajder <97719684+FakeCoder01@users.noreply.github.com>
2023-02-02 23:12:43 +0700

    Update part1.py

--- part1.py
+++ part1.py
@@ -15,7 +15,7 @@ soup = BeautifulSoup(response.text, 'html.parser')
 
 
 product_names = soup.find_all('span', class_='a-size-medium a-color-base a-text-normal')
-price_and_url = soup.find_all('a', class_='a-size-base a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal')
+urls = soup.find_all('a', class_='a-size-base a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal')
 review_and_rating = soup.find_all('div', class_='a-row a-size-small')
 prices = soup.find_all('div', class_='a-section a-spacing-none a-spacing-top-micro s-price-instructions-style')
 
@@ -26,7 +26,7 @@ with open('20_products.csv', 'w', newline='', encoding='utf-8-sig') as csvfile:
     csvwriter.writerow(["URL", "Name", "Price", "Rating", "Review"]) # Write the header row
 
     for i in range(20):
-        product_url = "https://amazon.in" + price_and_url[i].attrs['href'] 
+        product_url = "https://amazon.in" + urls[i].attrs['href'] 
         product_price = prices[i].select_one('.a-price-whole').text
         product_name = product_names[i].text
         product_rating = review_and_rating[i].text[:3]
@@ -36,4 +36,3 @@ with open('20_products.csv', 'w', newline='', encoding='utf-8-sig') as csvfile:
     csvfile.close()    
 
 print('Done. Data saved to 20_products.csv')
-
